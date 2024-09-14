49e0554a87e2b99878c87c966a10937e0ce2a3b2
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-09-14 20:29:01 +0600

    Add files via upload

---
+++ input_problem.py
@@ -0,0 +1,11 @@
+#Problems
+
+a=input("Enter the first Number: ")
+b=input("Enter the second Number: ")
+print(a+b)
+
+#Solutions: 
+
+a=input("Enter the first Number: ")
+b=input("Enter the second Number: ")
+print(int(a)+int(b))
