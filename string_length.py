b5e9771a15816932d037d10bc9d5a786dd6e3932
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-09-14 22:35:05 +0600

    Add files via upload

---
+++ string_length.py
@@ -0,0 +1,5 @@
+Name="Niher Ronjon Pramanik"
+print(len(Name))
+
+Name="Niher Ronjon Pramanik"
+print(Name[0:20])
---
+++ string_slicing.py
@@ -0,0 +1,11 @@
+Name= "Niher Ronjon Pramanik"
+n=len(Name)
+print(n)
+print(Name[0:19])
+print(Name[0:5])
+print(Name[0:7])
+
+
+#Negative slicing
+
+print(Name[0:-4])
