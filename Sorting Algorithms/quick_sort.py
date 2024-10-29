6f3d634f4ff1ba0eaa31ed63a2221f3d4eddac5e
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-10-29 19:48:35 +0600

    Add files via upload

---
+++ Sorting Algorithms/quick_sort.py
@@ -0,0 +1,29 @@
+#   quicksort using Python
+
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+def quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quicksort(array, low, pi - 1)
+        quicksort(array, pi + 1, high)
+
+data = [1, 7, 4, 1, 10, 9, -2]
+print("Unsorted Array")
+print(data)
+
+size = len(data)
+quicksort(data, 0, size - 1)
+
+print("Sorted Array in Ascending Order:")
+print(data)
