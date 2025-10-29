52964b94dd579648c3c6f2c2334a22c55e21ac10
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-10-29 15:13:17 +0600

    Create Sum_of_three_numbers.py

---
+++ Snakify Problem Solve/Sum_of_three_numbers.py
@@ -0,0 +1,7 @@
+"""Statement :
+Write a program that takes three numbers and prints their sum. Every number is given on a separate line.""""
+
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
